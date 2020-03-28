fd473c5a6ff93fd513185d0a7d757a05b616499f
Thomas Phillips <thomasphillips3@gmail.com>
2020-03-28 13:24:56 -0700

    Display the Canvas

--- app/src/main/java/com/thomasphillips3/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/thomasphillips3/minipaint/MyCanvasView.kt
@@ -1,8 +1,27 @@
 package com.thomasphillips3.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
-class MyCanvasView (context: Context) : View(context){
+private lateinit var extraCanvas: Canvas
+private lateinit var extraBitmap: Bitmap
 
+class MyCanvasView(context: Context) : View(context) {
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        if(::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
 }
